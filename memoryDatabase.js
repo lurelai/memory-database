56d453f8306e057cf293e8485de339be7e6521ba
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-02-05 13:34:43 -0300

    add a function to find in database

--- memoryDatabase.js
+++ memoryDatabase.js
@@ -1,5 +1,43 @@
 class Database{
+    static documents = new Map() //Collection Name, documents collection [ id, value... id, value... id, value ]
 
+    Collection = class{
+        constructor(collectionName = 'myCollection'){
+            Database.documents.set(collectionName, new Map())
+
+            this.name = collectionName
+            this.currentId = 0
+        }
+
+        create(add){
+            return new Promise((resolve, reject)=>{
+                try{
+                    Database.documents.get(this.name).set(this.currentId, add)
+                    this.currentId += 1
+
+                    resolve({message: 'user add', documents: Database.documents})
+                }catch(err){
+                    throw err
+                }
+            })
+        }
+
+        find(callBack){
+            return new Promise((resolve, reject)=>{
+                try{
+                    let a = Array.from(Database.documents.get(this.name).entries())
+
+                    for(let value of a){
+                        callBack(value[0], value[1])
+                    }
+
+                    resolve({mesage: 'okay'});
+                }catch(err){
+                    throw err
+                }
+            })
+        }
+    }
 }
 
 module.exports = Database;
--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const fastify = require('fastify')({
 })
 
 const memoryDatabase = require('./memoryDatabase');
+const db = new memoryDatabase();
+const collection = new db.Collection;
 
 (async ()=>{
-    console.log('ok')
+    let a = await collection.create({ name: 'lucas', idade: 10 })
+    let c = await collection.create({ name: 'pedro', idade: 10 })
+
+    await collection.find((id, values)=>{
+        if(values.name === 'lucas' && values.idade === 12)
+            console.log('ok')
+    })
 })()
 
 fastify.get('/', (req, reply)=>{
