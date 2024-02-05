88f11f4fc9cc6c9dbc4279507d6d3c6b3a59e9ae
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-02-05 14:24:44 -0300

    standardize the messages when an operation is successful

--- memoryDatabase.js
+++ memoryDatabase.js
@@ -2,27 +2,40 @@ class Database{
     static documents = new Map() //Collection Name, documents collection [ id, value... id, value... id, value ]
 
     Collection = class{
-        constructor(collectionName = 'myCollection'){
+        constructor(collectionName = 'myCollection', messages={}){
+            const {
+                _create = 'document created', // define the default message to when you create an document
+                _delete = 'document deleted', // define the default message to when you delete an document
+                _update = 'document updated', // define the default message to when you update an document
+                _list = 'collection listed',  // define the default message to when you list an collection
+            } = messages
+
             this.name = collectionName
             this.currentId = 0
+            this.messages = {
+                _create,
+                _delete,
+                _update,
+                _list,
+            }
 
             Database.documents.set(collectionName, new Map())
         }
 
-        create(add, message='document created'){
+        create(add){
             return new Promise((resolve, reject)=>{
                 try{
                     Database.documents.get(this.name).set(this.currentId, add)
                     this.currentId += 1
 
-                    return resolve({message})
+                    return resolve({message: this.messages._create})
                 }catch(err){
                     throw err
                 }
             })
         }
 
-        list(callBack, message='okay'){
+        list(callBack){
             return new Promise((resolve, reject)=>{
                 try{
                     let a = Array.from(Database.documents.get(this.name).entries())
@@ -31,14 +44,14 @@ class Database{
                         callBack(value[0], value[1])
                     }
 
-                    return resolve({message});
+                    return resolve({message: this.messages._list});
                 }catch(err){
                     throw err
                 }
             })
         }
 
-        delete(id, message='document deleted'){
+        delete(id){
             return new Promise((resolve, reject)=>{
                 try{
                     const tryIt = Database.documents.get(this.name).get(id)
@@ -48,14 +61,14 @@ class Database{
 
                     
                     Database.documents.get(this.name).delete(id)
-                    return resolve({message})
+                    return resolve({message: this.messages._delete})
                 }catch(err){
                     throw err
                 }
             })
         }
 
-        update(id, newValue, message='document updated'){
+        update(id, newValue){
             return new Promise((resolve, reject)=>{
                 let tryIt = Database.documents.get(this.name).get(id)
 
@@ -63,7 +76,7 @@ class Database{
                     return resolve({message: 'invalid id'})
 
                 Database.documents.get(this.name).set(id, newValue)
-                return resolve({message})
+                return resolve({message: this.messages._update})
             })
         }
     }
--- server.js
+++ server.js
@@ -7,21 +7,23 @@ const db = new memoryDatabase();
 const collection = new db.Collection('teste');
 
 (async ()=>{
-    await collection.create({ name: 'simao', idade: 10 })
+    let a = await collection.create({ name: 'simao', idade: 10 })
     await collection.create({ name: 'joao', idade: 15 })
     await collection.create({ name: 'ariel', idade: 12 })
     await collection.create({ name: 'judas', idade: 12 })
 
-    await collection.list((id, values)=>{
+    let b = await collection.list((id, values)=>{
         console.log(id, values)
     }) 
 
-    await collection.delete(0)
-    await collection.update(1, {name:'lucas', idade: 16})
+    let c = await collection.delete(0)
+    let d = await collection.update(1, {name:'lucas', idade: 16})
 
     await collection.list((id, values)=>{
         console.log(id, values)
     })
+
+    console.log(a, b, c, d)
 })()
 
 fastify.get('/', (req, reply)=>{
