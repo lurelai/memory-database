ec86457edd36a2d131e4feff28c5ca34ca9b46c9
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-02-04 23:50:51 -0300

    add a way to create collections

--- memoryDatabase.js
+++ memoryDatabase.js
@@ -1,8 +1,30 @@
 class Database{
     static collections = new Map()
 
-    set newCollection(collectionName){
-        Database.collections.set(collectionName) 
+    constructor(dbName){
+        this.name = dbName;
+    }
+
+    newCollection(collectionName = 'yourCollection'){
+        class Collection{
+            static documents = new Map()
+
+            constructor(){
+                this.name = collectionName
+                this.currentId = 0
+            }
+
+            set create(obj){
+                console.log(Collection)
+            }
+        }
+
+        // Create a instance of this class
+        let collectionInstance = new Collection;
+
+        // Set the instance at the db and return the same instance
+        Database.collections.set(collectionName, collectionInstance)
+        return collectionInstance
     }
 }
 
--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const fastify = require('fastify')({
     logger: true
 })
 
+const memoryDatabase = require('./memoryDatabase');
+const db = new memoryDatabase('Escola');
+
+const collection = db.newCollection()
+collection.create = 'ok'
+
+console.log('Database: ', db, '\nCollection: ', collection)
+
 fastify.get('/', (req, reply)=>{
     return 'ok'
 })
